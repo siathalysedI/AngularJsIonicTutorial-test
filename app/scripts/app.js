eac359e5e7fdf0305629eeabe3ca3b177f700c80
Pierre Caserta <pierre.caserta@gmail.com>
2015-03-22 18:58:43 +0100

    initial commit
    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+angular.module('IonicTut', ['ionic', 'config', 'IonicTut.controllers', 'infinite-scroll', 'ngMaterial'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+
+  $stateProvider
+  .state('intro', {
+    url: '/',
+    templateUrl: 'templates/intro.html',
+    controller: 'IntroCtrl'
+  })
+  .state('main', {
+    url: '/main',
+    templateUrl: 'templates/main.html',
+    controller: 'MainCtrl'
+  });
+
+  $urlRouterProvider.otherwise('/');
+
+  $mdThemingProvider.theme('default')
+            .primaryPalette('pink')
+            .accentPalette('orange');
+
+});
+  
